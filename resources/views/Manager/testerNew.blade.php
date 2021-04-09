67f8ea20d1bb2623afb5c766f9211b9d26ca8049
DESKTOP-FP75HIB\User <sidi.meisanjaya2013@gmail.com>
2021-04-09 12:12:15 +0900

    space

--- resources/views/Manager/testerNew.blade.php
+++ resources/views/Manager/testerNew.blade.php
@@ -55,7 +55,9 @@
                                             </label>
 
                                             <div class="col-md-6">
-                                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+                                                <input id="password" type="password" 
+                                                    class="form-control @error('password') is-invalid @enderror" 
+                                                    name="password" required autocomplete="new-password">
 
                                                 @error('password')
                                                     <span class="invalid-feedback" role="alert">
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -56,7 +56,10 @@
                                             </label>
 
                                             <div class="col-md-6">
-                                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+                                                <input id="password" type="password" 
+                                                    class="form-control @error('password') is-invalid @enderror"
+                                                    name="password" required 
+                                                    autocomplete="new-password">
 
                                                 @error('password')
                                                     <span class="invalid-feedback" role="alert">
